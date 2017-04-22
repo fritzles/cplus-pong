97c9c16f4e1e13a23d2f3bf56bad035cbf5901f6
Michael <mdfritz@uvm.edu>
2017-04-21 20:42:39 -0400

    lmao

--- Pong/Player.cpp
+++ Pong/Player.cpp
@@ -7,10 +7,76 @@ Jordan Guzak, Michael Fritz, Chris Bracky
 
 #include "Player.h"
 
-Player::Player() {
+#include <iostream>
+#include <fstream>
 
+
+Player::Player() : name(""), gamesPlayed(0), gamesWon(0) {
 }
 
 Player::~Player() {
+}
 
+void Player::saveData() {
+    if(name != "") {
+        fstream player;
+        player.open(name + ".txt", fstream::out | fstream::trunc);
+        player << gamesWon << " " << gamesPlayed;
+    }else{
+        cout << "You must first select a player." << endl;
+    }
+}
+
+void Player::loadData(string playerName) {
+    name = playerName;
+
+    fstream player;
+    string filename = playerName + ".txt";
+    player.open(filename);
+
+    // If file does not exist, Create new file
+    if ( !player )
+    {
+        player.open(filename,  fstream::in | fstream::out | fstream::trunc);
+        player << gamesWon << " " << gamesPlayed;
+        player.close();
+
+    }
+    else
+    {    // use existing file
+        int test, test2;
+        player >> gamesWon;
+        player >> gamesPlayed;
+        player.close();
+        cout<<"\n";
+
+    }
+}
+
+string Player::getName() const{
+    return name;
+}
+
+int Player::getGamesPlayed() const{
+    return gamesPlayed;
+}
+
+int Player::getGamesWon() const{
+    return gamesWon;
+}
+
+int Player::getGamesLost() const{
+    return gamesPlayed - gamesWon;
+}
+
+void Player::setName(string name){
+    loadData(name);
+}
+
+void Player::setGamesPlayed(int games){
+    gamesPlayed = games;
+}
+
+void Player::setGamesWon(int games){
+    gamesWon = games;
 }
--- Pong/Player.h
+++ Pong/Player.h
@@ -8,14 +8,34 @@ Jordan Guzak, Michael Fritz, Chris Bracky
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <string>
+
+using namespace std;
+
 class Player
 {
 public:
     Player();
     ~Player();
 
+    void saveData();
+
+    void loadData(string);
+
+    string getName() const;
+    int getGamesPlayed() const;
+    int getGamesWon() const;
+    int getGamesLost() const;
+
+    void setName(string);
+    void setGamesPlayed(int);
+    void setGamesWon(int);
+
 private:
 
+    string name;
+    int gamesPlayed;
+    int gamesWon;
 };
 
 #endif // !PLAYER_H
